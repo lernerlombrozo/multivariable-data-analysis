1e10e5348d743e328e923d503c8d4d26d5a2a130
David Lerner <lernerlombrozo@gmail.com>
2018-10-14 15:10:32 -0600

    now does multiple multivariate regression

--- Assignments/Assignment 2/Matlab/Problem1.m
+++ Assignments/Assignment 2/Matlab/Problem1.m
@@ -1,6 +1,6 @@
 % * Needs to be manually modiffied if you add a new variable
 % **  Wont display all data if you add a new variable
-close all; clear all; clc; 
+close all; clear; clc; 
 %reading file
 filename = 'dataSet.csv'; %Selected csv file must be in same folder as current File
 DataSet = csvread(filename,1,0);
@@ -8,45 +8,13 @@ DataSet = csvread(filename,1,0);
 %Taking the desired columns
 X = [DataSet(:,2),DataSet(:,3),DataSet(:,4)]; % *
 Y = [DataSet(:,5),DataSet(:,6)]; % *
-[sampleN, samples]= size(X);
-
-
+[n,r] = size(X);
+[~,m] = size(Y);
 %X = [ 1 2 -1; 2 4 -2; 3 6 -3] %Uncomment for testing an easy distribution
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%% Part A: Statistics %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % mvregress for Multiple Multivariate Regression
-load('flu')
+X = [ones(n,1) X];
+[beta,Sigma,E,CovB,logL] = mvregress(X,Y)
 
-%{
-figure;
-scatter3(X(:,2),X(:,3),Y(:,1),'filled')
-hold on
-x1fit = min(X(:,2)):0.1:max(X(:,2));
-x2fit = min(X(:,3)):0.1:max(X(:,3));
-[X1FIT,X2FIT] = meshgrid(x1fit,x2fit);
-YFIT = b(1) + b(2)*X1FIT + b(3)*X2FIT + b(4)*X1FIT.*X2FIT; %Regression Model
-mesh(X1FIT,X2FIT,YFIT);
-xlabel('x1');
-ylabel('x2');
-zlabel('y1');
-view(50,10);
-hold off
 
-figure;
-scatter3(X(:,2),X(:,3),Y(:,2),'filled')
-hold on
-x1fit = min(X(:,2)):0.1:max(X(:,2));
-x2fit = min(X(:,3)):0.1:max(X(:,3));
-[X1FIT,X2FIT] = meshgrid(x1fit,x2fit);
-YFIT = b(1) + b(2)*X1FIT + b(3)*X2FIT + b(4)*X1FIT.*X2FIT; %Regression Model
-mesh(X1FIT,X2FIT,YFIT);
-xlabel('x1');
-ylabel('x2');
-zlabel('y2');
-view(50,10);
-hold off
-%}
-
-%Confidence intervals of the estimate, default alpha = 0.05
-disp('Confidence intervals of the estimate');
-disp(bint);
--- Assignments/Assignment 2/Matlab/mregressTest.m
+++ Assignments/Assignment 2/Matlab/mregressTest.m
@@ -1,16 +1,16 @@
-close all; clear all; clc; 
+close all; clear; clc; 
 load('flu');
 Y = double(flu(:,2:end-1));
 [n,d] = size(Y);
 x = flu.WtdILI;
 
+%{
 figure;
 regions = flu.Properties.VarNames(2:end-1);
 plot(x,Y,'x')
 legend(regions,'Location','NorthWest')
 
 X = cell(n,1);
-
 for i = 1:n
 	X{i} = [eye(d) repmat(x(i),d,1)];
 end
@@ -25,4 +25,41 @@ h = plot(x,Y,'x',xx,fits,'-');
 for i = 1:d
 	set(h(d+i),'color',get(h(i),'color'));
 end
+legend(regions,'Location','NorthWest');
+
+X = cell(n,1);
+for i = 1:n
+    X{i} = [eye(d) x(i)*eye(d)];
+end
+[beta,Sigma] = mvregress(X,Y,'algorithm','cwls');
+
+B = [beta(1:d)';beta(d+1:end)'];
+xx = linspace(.5,3.5)';
+fits = [ones(size(xx)),xx]*B;
+
+figure;
+h = plot(x,Y,'x',xx,fits,'-');
+for i = 1:d
+	set(h(d+i),'color',get(h(i),'color'));
+end
+
+regions = flu.Properties.VarNames(2:end-1);
+legend(regions,'Location','NorthWest');
+
+%}
+
+X = [ones(size(x)),x];
+[beta,Sigma,E,CovB,logL] = mvregress(X,Y);
+
+B = beta;
+xx = linspace(.5,3.5)';
+fits = [ones(size(xx)),xx]*B;
+
+figure;
+h = plot(x,Y,'x', xx,fits,'-');
+for i = 1:d
+    set(h(d+i),'color',get(h(i),'color'));
+end
+
+regions = flu.Properties.VarNames(2:end-1);
 legend(regions,'Location','NorthWest');
